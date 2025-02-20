512191157d1810b14be7d470903af0ea9b885ef7
Gabriel de Almeida Mangueira <gabrielam1729@gmail.com>
2025-02-20 14:50:01 -0300

    refactoração do app para deixar mais limpo

--- app.py
+++ app.py
@@ -1,21 +1,50 @@
+import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flasgger import Swagger
+from models import db
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+def init_extensions(app):
+    # Inicializa o SQLAlchemy
+    db.init_app(app)
+    
+    swagger_config = {
+        "headers": [],
+        "specs": [
+            {
+                "endpoint": "apispec_1",
+                "route": "/apispec_1.json",
+                "rule_filter": lambda rule: True,  # inclui todas as rotas
+                "model_filter": lambda tag: True,  # inclui todos os modelos
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        "swagger_ui": True,
+        "specs_route": "/apidocs/"
+    }
+    Swagger(app, config=swagger_config)
 
+def register_blueprints(app):
+    from controllers.auth import auth_bp
+    from controllers.user import user_bp
+    from controllers.post import post_bp
 
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(post_bp)
 
-from controllers.auth import auth_bp
-from controllers.user import user_bp
-from controllers.post import post_bp
+def create_app():
+    app = Flask(__name__)
 
+    # Configurações da aplicação
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///mydb.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.secret_key = os.getenv('SECRET_KEY', 'SUA_CHAVE_SECRETA')
 
-app.register_blueprint(auth_bp)
-app.register_blueprint(user_bp)
-app.register_blueprint(post_bp)
+    init_extensions(app)
+    register_blueprints(app)
+    
+    return app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    flask_app = create_app()
+    flask_app.run(debug=True)
